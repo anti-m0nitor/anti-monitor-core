63c50c6148333946baa6e7ac3e9188fc0c97f887
padovese <bpadovese@uolinc.com>
2021-03-31 16:55:54 -0300

    test: checking if 1 is equals to 1

--- src/test/java/com/antimonitor/core/CoreApplicationTests.java
+++ src/test/java/com/antimonitor/core/CoreApplicationTests.java
@@ -3,11 +3,18 @@ package com.antimonitor.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CoreApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void someTest(){
+		assertEquals(1, 1);
 	}
 
 }
