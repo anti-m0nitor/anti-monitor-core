612bd653d48f3c76fefa79642fd8774cba8a5b41
jojoccp <jonatas.ccp@gmail.com>
2021-04-01 16:13:17 -0300

    Criado dois novos end points, findestablishment e deleteestablishment

--- src/main/java/com/antimonitor/core/controller/EstablishmentController.java
+++ src/main/java/com/antimonitor/core/controller/EstablishmentController.java
@@ -7,11 +7,14 @@ import com.antimonitor.core.repository.EstablishmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class EstablishmentController {
@@ -23,13 +26,8 @@ public class EstablishmentController {
     @ResponseStatus(HttpStatus.CREATED)
     public EstablishmentResponseDTO createEstablishment(@RequestBody EstablishmentRequestDTO establishmentRequestDTO) {
 
-
-
         return new EstablishmentResponseDTO("Establishment was successfully created.");
-
     }
-
-
 
     @GetMapping("/establishment")
     public List<Establishment> listEstablishments() {
@@ -38,24 +36,29 @@ public class EstablishmentController {
 
     }
 
-
-
     @PostMapping("/createestablishment")
     public Establishment createEstablishment(@RequestBody Establishment establishment) {
-
-//        establishmentRequestDTO.setId(uuidRandom());
-//        establishmentRequestDTO.setName("Jonatas");
 
         return establishmentRepository.save(establishment);
     }
 
 
-    public static UUID uuidRandom() {
-        UUID uuid = UUID.randomUUID();
-        String uuidAsString = uuid.toString();
 
-        return uuid;
+
+    @GetMapping("/findestablishment/{id}")
+    public Establishment findEstablishment(@PathVariable("id") UUID id){
+
+        return establishmentRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
+
+    @DeleteMapping("/deleteestablishment/{id}")
+    public void deleteEstablishment(@PathVariable("id") UUID id) {
+
+        establishmentRepository.delete(findEstablishment(id));
+    }
+
+
+
 
 
 
